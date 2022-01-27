94f3b67f4db603d7192d60582312c14c2ce69e78
Suki Sahota <suki@Sukis-MacBook-Air.local>
2022-01-26 19:17:40 -0800

    corrects mask

--- Parity.cpp
+++ Parity.cpp
@@ -13,7 +13,7 @@ public:
      */
     uint32_t findParity(uint32_t val) {
         uint32_t num_bits = 16;
-        uint32_t mask = 0xFFF;
+        uint32_t mask = 0xFFFF;
 
         uint32_t left_half = val >> num_bits;
         uint32_t right_half = val & mask;
